d0dfbe06c9a8ecbc4e8c88328f6a4fada2c3813d
ian chua <icychua@gmail.com>
2024-11-05 14:53:56 +0800

    add images to methodology

--- pages/1_Methodology.py
+++ pages/1_Methodology.py
@@ -7,16 +7,19 @@ st.title("Methodology")
 st.write('''
 Information were all taken from the CMPB website: https://cmpb.gov.sg''')
 
-expand = st.expander("AMA about CMPB")
-expand.markdown("""
+with st.expander("AMA about CMPB"):
+    st.write("""
 - Information was retrieved from the various CMPB websites about pre-enlistee info
 - The context was fed to the LLM
 - Process user queries using only the context provided.
-""")
+             """)
+    st.image("./cmpb ama methodology.png")
 
-expand2 = st.expander("Vocation chooser")
-expand2.markdown("""
+with st.expander("Vocation chooser"):
+    st.write("""
 - Information was retrieved from the vocation details handbook, found at https://www.cmpb.gov.sg/ns-vocations/
 - Vocation details were processed into csv
 - Then fed into LLM 
-- User descriptions were then used to assess their vocation suitability.""")
+- User descriptions were then used to assess their vocation suitability.
+             """)
+    st.image("./vocation chooser methodology.png")
